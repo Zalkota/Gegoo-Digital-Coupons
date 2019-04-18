afe0a96debe29f48213a2de4f1e38ad3424f7c28
zalkota <dominicmazzola@gmail.com>
2019-04-17 23:33:38 -0400

    gmail crediantials

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -10,7 +10,7 @@ SECRET_KEY = '#^57b45ap)gvbl@#@f5v1uqbv)1x3na2ct@xe$ql^r_ds#g0ap'
 ALLOWED_HOSTS = ['*']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER ='hello@modwebservices.com'
+EMAIL_HOST_USER ='dominic@modwebservices.com'
 EMAIL_HOST_PASSWORD = '$Django28' #TODO This should probably go to a python variable
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
