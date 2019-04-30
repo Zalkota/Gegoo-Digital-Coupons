0ea05a2f5b7f68253a6c0b22ab5b19c278ecb424
zalkota <dominicmazzola@gmail.com>
2019-04-29 22:47:28 -0400

    drift update

--- mysite/mysite/static/js/drift.js
+++ mysite/mysite/static/js/drift.js
@@ -1,3 +1,4 @@
+<!-- Start of Async Drift Code -->
 
 "use strict";
 
@@ -5,7 +6,7 @@
   var t = window.driftt = window.drift = window.driftt || [];
   if (!t.init) {
     if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
     t.factory = function(e) {
       return function() {
         var n = Array.prototype.slice.call(arguments);
@@ -22,4 +23,6 @@
   }
 }();
 drift.SNIPPET_VERSION = '0.3.1';
-drift.load('kwrmh6ugxzt5');
+drift.load('3vy5pu5p8h38');
+
+<!-- End of Async Drift Code -->
