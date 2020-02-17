88f667f3e14b773de396d7f3f6556d46dfe00f24
zalkota <dominicmazzola@gmail.com>
2020-02-16 19:01:59 -0500

    error logging

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,8 +8,23 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 import os
 
+#Error logging
+import time
+import traceback
+import signal
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
