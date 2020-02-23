735fc17d8e4c2cde2325641cf322dc2afc2982c8
zalkota <dominicmazzola@gmail.com>
2020-02-23 02:36:48 -0500

    Late Night changes

--- mysite/compose/django/entrypoint.sh
+++ mysite/compose/django/entrypoint.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
-set -e
-cmd="$@"
 
-# #TODO: #FIXME: Get this up to date with https://github.com/pydanny/cookiecutter-django/tree/3853094221ae6dfe1e15b7a4fb35fc36d12bcba4
-# https://github.com/pydanny/cookiecutter-django/tree/179adb4f30dfeeace03e6c5fe876e473b0737d02
-# This entrypoint is used to play nicely with the current cookiecutter configuration.
-# Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
-# environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
-# does all this for us.
-# export REDIS_URL=redis://redis:6379
+# Prepare log files and start outputting logs to stdout
+touch ./logs/gunicorn.log
+touch ./logs/gunicorn-access.log
+tail -n 0 -f ./logs/gunicorn*.log &
 
-# the official postgres image uses 'postgres' as default user if not set explictly.
-if [ -z "$POSTGRES_USER" ]; then
-    export POSTGRES_USER=postgres
-fi
+export DJANGO_SETTINGS_MODULE=mysite.settings.dev
 
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
-
-# export CELERY_BROKER_URL=$REDIS_URL/0
-
-
-function postgres_ready(){
-python << END
-import sys
-import psycopg2
-try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
-except psycopg2.OperationalError:
-    sys.exit(-1)
-sys.exit(0)
-END
-}
-
-until postgres_ready; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - continuing..."
-exec $cmd
+exec gunicorn mysite.wsgi:application \
+    --name mysite_django_1 \
+    --bind 0.0.0.0:8000 \
+    --workers 5 \
+    --log-level=info \
+    --log-file=./logs/gunicorn.log \
+    --access-logfile=./logs/gunicorn-access.log \
+"$@"
