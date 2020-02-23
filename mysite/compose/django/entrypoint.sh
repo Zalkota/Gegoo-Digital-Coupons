2e0d8d4e22dfd06af3924ab59cb53f20257e9147
zalkota <dominicmazzola@gmail.com>
2020-02-23 13:47:25 -0500

    entrypoint

--- mysite/compose/django/entrypoint.sh
+++ mysite/compose/django/entrypoint.sh
@@ -5,13 +5,13 @@ touch ./logs/gunicorn.log
 touch ./logs/gunicorn-access.log
 tail -n 0 -f ./logs/gunicorn*.log &
 
-export DJANGO_SETTINGS_MODULE=mysite.settings.dev
+# export DJANGO_SETTINGS_MODULE=mysite.settings.dev
 
-exec gunicorn mysite.wsgi:application \
-    --name django \
-    --bind 0.0.0.0:8000 \
-    --workers 5 \
-    --log-level=info \
-    --log-file=./logs/gunicorn.log \
-    --access-logfile=./logs/gunicorn-access.log \
+# exec gunicorn mysite.wsgi:application \
+#     --name django \
+#     --bind 0.0.0.0:8000 \
+#     --workers 5 \
+#     --log-level=info \
+#     --log-file=./logs/gunicorn.log \
+#     --access-logfile=./logs/gunicorn-access.log \
 "$@"
