34789b528164f2384dd84042ff365885cb2ae9b6
zalkota <dominicmazzola@gmail.com>
2020-03-04 23:52:18 -0500

    serviceUrl: "https://www.printrender.com:8000/search/autocomplete/",

--- mysite/mysite/static/js/search/our_search_code.js
+++ mysite/mysite/static/js/search/our_search_code.js
@@ -2,7 +2,8 @@ $(function () {
     'use strict';
 
     $('#q').autocomplete({
-    serviceUrl: "http://www.printrender.com:8000/search/autocomplete/",
+    // serviceUrl: "http://127.0.0.1:8000/search/autocomplete/",
+    serviceUrl: "https://www.printrender.com:8000/search/autocomplete/",
     minChars: 2,
     dataType: 'json',
     type: 'GET',
