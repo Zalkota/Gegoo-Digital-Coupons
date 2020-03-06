ab7c10b4ba976926f5dfa83f1988a2bf02bbdbb9
zalkota <dominicmazzola@gmail.com>
2020-03-05 19:22:02 -0500

    test

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 import os
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1', '0.0.0.0').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 ADMINS = [('ModTechnology', 'dominic@modwebservices.com')]
 
