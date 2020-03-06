c20b4cca4f2be64a69304b8d4a1cac6ddb9a8065
zalkota <dominicmazzola@gmail.com>
2020-03-05 19:15:04 -0500

    elastic search for the answer

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 import os
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1', '0.0.0.0').split(',')
 
 ADMINS = [('ModTechnology', 'dominic@modwebservices.com')]
 
